92fb2f46fd83ee3e563e0a7b8cbeff59ad888f8f
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-06-07 20:24:17 +0300

    Update joinvc.py

--- Branded/plugins/vcbot/joinvc.py
+++ Branded/plugins/vcbot/joinvc.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pytgcalls.exceptions import NoActiveGroupCall
 
 
-@app.on_message(cdx(["join", "joinvc"]) & ~filters.private)
+@app.on_message(cdx(["join", "ekle", "joinvc"]) & ~filters.private)
 @sudo_users_only
 async def join_vc(client, message):
     chat_id = message.chat.id
@@ -12,48 +12,48 @@ async def join_vc(client, message):
     if_chat = a_calls.get(chat_id)
     if if_chat:
         return await eor(
-            message, "**Already Joined VC!**"
+            message, "**VC'ye Zaten Katıldım!**"
         )
     if not if_chat:
         try:
             await call.play(chat_id)
             return await eor(
-                message, "**Joined VC!**"
+                message, "**sesliyi açtım !**"
             )
         except NoActiveGroupCall:
             return await eor(
-                message, "**No Active VC!**"
+                message, "** VC'ye katıldı!**"
             )
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"hata: {e}")
             pass
         
 
 
-@app.on_message(cdz(["cjoin", "cjoinvc"]))
+@app.on_message(cdz(["cjoin", "caç", "cjoinvc"]))
 @sudo_users_only
 async def join_vc_(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
     if chat_id == 0:
         return await eor(message,
-            "**🥀 No Stream Chat Set❗**"
+            "**🥀 Yayın Sohbet Seti Yok❗**"
     )
     a_calls = await call.calls
     if_chat = a_calls.get(chat_id)
     if if_chat:
         return await eor(
-            message, "**Already Joined VC!**"
+            message, "**VC'ye Zaten Katıldım!**"
         )
     if not if_chat:
         try:
             await call.play(chat_id)
             return await eor(
-                message, "**Joined VC!**"
+                message, "**VC'ye katıldı!**"
             )
         except NoActiveGroupCall:
             return await eor(
-                message, "**No Active VC!**"
+                message, "**Aktif VC Yok!**"
             )
         except Exception as e:
             print(f"Error: {e}")
