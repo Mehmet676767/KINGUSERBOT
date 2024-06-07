6e9c1768eb806436715bcd7c89020e420094a0df
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-06-07 20:34:26 +0300

    Update pause.py

--- Branded/plugins/vcbot/pause.py
+++ Branded/plugins/vcbot/pause.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pytgcalls.exceptions import GroupCallNotFound
 
 
-@app.on_message(cdx(["pse", "pause"]) & ~filters.private)
+@app.on_message(cdx(["dur", "son"]) & ~filters.private)
 @sudo_users_only
 async def pause_stream(client, message):
     chat_id = message.chat.id
@@ -12,38 +12,38 @@ async def pause_stream(client, message):
         a = await call.get_call(chat_id)
         if a.status == "playing":
             await call.pause_stream(chat_id)
-            await eor(message, "**Stream Paused!**")
-        elif a.status == "paused":
-            await eor(message, "**Already Paused!**")
-        elif a.status == "not_playing":
-            await eor(message, "**Nothing Streaming!**")
+            await eor(message, "**Akış Duraklatıldı!**")
+        elif a.status == "son", "dur":
+            await eor(message, "**Zaten Duraklatıldı!**")
+        elif a.status == "oynamıyorum":
+            await eor(message, "**Hiçbir Şey Yayınlanmıyor!**")
     except GroupCallNotFound:
-        await eor(message, "**I am Not in VC!**")
+        await eor(message, "**VC'de değilim!**")
     except Exception as e:
         print(f"Error: {e}")
 
 
-@app.on_message(cdz(["cpse", "cpause"]))
+@app.on_message(cdz(["cpse", "cson", "cpause"]))
 @sudo_users_only
 async def pause_stream_(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
     if chat_id == 0:
         return await eor(message,
-            "**🥀 No Stream Chat Set❗**"
+            "**🥀 Yayın Sohbet Seti Yok❗**"
     )
     try:
         a = await call.get_call(chat_id)
-        if a.status == "playing":
+        if a.status == "şarkı başladı":
             await call.pause_stream(chat_id)
-            await eor(message, "**Stream Paused!**")
+            await eor(message, "**akış durduruldu!**")
         elif a.status == "paused":
-            await eor(message, "**Already Paused!**")
-        elif a.status == "not_playing":
-            await eor(message, "**Nothing Streaming!**")
+            await eor(message, "**akış zaten durdururldu!**")
+        elif a.status == "oynamıyorum":
+            await eor(message, "**akış kapalı lütfen sesliyi açın!**")
     except GroupCallNotFound:
-        await eor(message, "**I am Not in VC!**")
+        await eor(message, "**VC'de değilim!**")
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"hata: {e}")
 
   
